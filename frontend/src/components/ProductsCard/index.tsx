0d7ce0bd40a77320e478101ed1f42c1558aa04fb
laura-ml <78579532+laura-ml@users.noreply.github.com>
2024-07-26 19:26:52 +0200

    act

--- frontend/src/components/ProductsCard/index.tsx
+++ frontend/src/components/ProductsCard/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 function ProductsCard({ product }: IProductProps) {
     return (
-        <div className="border-2 border-[#800020] p-4 rounded-lg w-[300px] h-[400px] flex flex-col mx-auto bg-gradient-to-r from-[#4b0026] via-[#800020] to-[#a52a2a]">
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md p-4 ">
             <div className="p-4 flex items-center justify-center flex-grow">
                 <div className="relative w-32 h-32">
                     <Image
@@ -19,13 +19,13 @@ function ProductsCard({ product }: IProductProps) {
                 <h5 className="text-xl font-bold text-center mb-2 text-white">{product.name}</h5>
                 <div className="mb-2 space-y-1">
                     <span className="block text-lg text-gray-300 font-semibold">
-                        Type: <span className="font-normal">{product.type}</span>
+                        Tipo: <span className="font-normal">{product.type}</span>
                     </span>
                     <span className="block text-lg text-gray-300 font-semibold">
-                        Store: <span className="font-normal">{product.store}</span>
+                        Bodega: <span className="font-normal">{product.store}</span>
                     </span>
                     <span className="block text-lg text-gray-300 font-semibold">
-                        Price: <span className="font-normal">{`$${product.price}`}</span>
+                        <span className="font-normal">{`$${product.price}`}</span>
                     </span>
                 </div>
                 <div className="flex w-full justify-end">
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      champagne: {
+      DEFAULT: '#D4AF37',
+      },
+    },
   },
   plugins: [],
 };
